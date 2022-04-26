66e9e8134b411e6536ddca83f445a16bffe02d51
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-04-26 09:12:36 +0600

    complete price section in the card

--- src/component/Service/Service.js
+++ src/component/Service/Service.js
@@ -16,13 +16,14 @@ const Service = () => {
                     <h1 className="text-center mb-5 text-light">HAVE A LOOK AT OUR SERVICES</h1>
                     {
                         service.map(services =>
-                            <div className="col-lg-4 mb-3" >
+                            <div key={services.id} className="col-lg-4 mb-3" >
                                 <Card className="text-center text-light" style={{ backgroundColor: "darkcyan" }}>
                                     <Card.Img variant="top" src={services.image} />
                                     <Card.Body>
                                         <Card.Title>{services.details}</Card.Title>
                                         <Card.Text>
-                                            {services.description}
+                                            <h4> {services.description}</h4>
+                                            <h5>{services.price}</h5>
                                         </Card.Text>
                                         <Link to='/checkout'>
                                             <Button variant="primary">Check Out</Button>
